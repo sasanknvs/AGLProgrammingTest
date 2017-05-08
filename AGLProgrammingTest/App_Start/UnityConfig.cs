2dcb0e9208d4f95a60897bf4c91aec8a8c5913c5
sasanknvs <sasanknvs@gmailcom>
2017-05-08 22:00:57 +1000

    cleaned up the code

--- AGLProgrammingTest/App_Start/UnityConfig.cs
+++ AGLProgrammingTest/App_Start/UnityConfig.cs
@@ -15,15 +15,9 @@ namespace AGLProgrammingTest
         {
 			var container = new UnityContainer();
 
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-
-            // e.g. container.RegisterType<ITestService, TestService>();
             container.RegisterType<IPetsBusinesslogic, PetsBusinesslogic>();
             container.RegisterType<IPetsDataAccess, PetsDataAccess>();
-            //container.RegisterType<IRestClient, RestClient>();
             container.RegisterType<IRestClient, RestClient>(new InjectionConstructor());
-            
 
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
